ae6745405b2ebcd9a3657ca508f2d5417138148c
crystal <crstalokd@gmail.com>
2022-01-30 17:28:08 +0100

    DRF-api

--- library_project/settings.py
+++ library_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'books.apps.BooksConfig', 
+    'rest_framework',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
